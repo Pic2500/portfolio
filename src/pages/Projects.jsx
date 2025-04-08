dc93becd00a3851b28ed146de831c4de2fcf9bc9
unknown <pintar.domagoj@gmail.com>
2025-04-08 19:26:02 +0200

    change to projects fade-in-up

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -35,7 +35,7 @@ const projects = [
 
 function Projects() {
   return (
-    <div className="container mt-5 ">
+    <div className="container mt-5 fade-in-up">
       <h1 className="text-center mb-4">My Projects</h1>
       <div className="row">
         {projects.map((project) => (
